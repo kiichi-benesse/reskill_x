f7ff1683fe3bdb2948fa501f736ae8039fa7de8f
kiichi-benesse <kiichi.watanabe@mail.benesse.co.jp>
2023-07-31 15:37:58 +0900

    はじめてのこみっと

---
+++ lib/constant/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const purple = Color(0xFFA435F0);
+const black = Color(0xFF1C1D1F);
+const white = Color(0xFFFFFFFF);
+const grey = Color(0xff727272);
+const lightGrey = Color(0xffbdbdbd);
+const orange = Color(0xFFFB5012);
+const lightBlue = Color(0xFF1098F7);
